72068c78a504ef06135908d725ec6a87c619c280
naahnoey <naahyin20@sookmyung.ac.kr>
2024-01-06 21:37:50 +0900

    Feat: link profile menu with fragment

--- app/src/main/java/com/example/jakdangmodok/MainActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/MainActivity.kt
@@ -2,23 +2,44 @@ package com.example.jakdangmodok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import com.example.jakdangmodok.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val fragments: List<Fragment> by lazy {
-        listOf(HomeFragment(), SubscribeFragment(), AddFragment(), BookFragment(), GroupFragment())
+        listOf(HomeFragment(), SubscribeFragment(), AddFragment(), BookFragment(), GroupFragment(), ProfileFragment())
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
         setBottomNavigationView()
 
         if (savedInstanceState == null) {
             binding.bottomNavigation.selectedItemId = R.id.menu_home
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_top, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_profile -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_container, fragments[5]).commit()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
