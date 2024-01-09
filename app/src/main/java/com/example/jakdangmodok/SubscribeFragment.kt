0c75ccca860fef2c9cb095f96426dfdcd2a3d505
naahnoey <naahyin20@sookmyung.ac.kr>
2024-01-09 20:27:56 +0900

    Design: separate subscribe fragment

--- app/src/main/java/com/example/jakdangmodok/SubscribeFragment.kt
+++ app/src/main/java/com/example/jakdangmodok/SubscribeFragment.kt
@@ -5,48 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.jakdangmodok.databinding.FragmentSubscribeBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SubscribeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SubscribeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private val binding by lazy { FragmentSubscribeBinding.inflate(layoutInflater) }
+    private val tabMenuList = listOf("책", "모임")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    }
+
+    class SubscribePagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+        val fragments: List<Fragment>
+
+        init {
+            fragments = listOf(BookFragment(), GroupFragment())
         }
+
+        override fun getItemCount(): Int = fragments.size
+
+        override fun createFragment(position: Int): Fragment = fragments[position]
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_subscribe, container, false)
+        binding.viewPager.adapter = SubscribePagerAdapter(this)
+        binding.viewPager.isSaveEnabled = false
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = tabMenuList[position]
+        }.attach()
+
+        return binding.root
     }
 
+    /*
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SubscribeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             SubscribeFragment().apply {
@@ -56,4 +53,5 @@ class SubscribeFragment : Fragment() {
                 }
             }
     }
+    */
 }
