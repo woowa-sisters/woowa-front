167041ce99932df4cff97293bcee0461a0c87d2c
naahnoey <naahyin20@sookmyung.ac.kr>
2024-01-11 21:40:28 +0900

    Design: add searchView and spinner

--- app/src/main/java/com/example/jakdangmodok/GroupFragment.kt
+++ app/src/main/java/com/example/jakdangmodok/GroupFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.jakdangmodok.databinding.FragmentGroupBinding
 
 class GroupFragment : Fragment() {
 
-    val groupList: ArrayList<String> = ArrayList<String>()
+    private val filterList: Array<String> = arrayOf("최신순", "마감순", "거리순")
+    private val groupList: ArrayList<String> = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +24,12 @@ class GroupFragment : Fragment() {
     ): View? {
         val binding = FragmentGroupBinding.inflate(inflater, container, false)
 
+        val filterAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, filterList)
+        filterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
         binding.recyclerviewGroup.layoutManager = LinearLayoutManager(activity)
         binding.recyclerviewGroup.adapter = GroupAdapter(groupList)
+        binding.spinnerGroup.adapter = filterAdapter
 
         return binding.root
     }
