16882c519311ee4c5e1051dd67cb63d8992a561c
naahnoey <naahyin20@sookmyung.ac.kr>
2024-01-16 22:26:14 +0900

    Feat: link button with activity (profile edit + notification settings)

--- app/src/main/java/com/example/jakdangmodok/ProfileActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/ProfileActivity.kt
@@ -1,11 +1,30 @@
 package com.example.jakdangmodok
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.jakdangmodok.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityProfileBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(binding.root)
+
+        buttonClick()
     }
+
+    private fun buttonClick() {
+        binding.buttonProfileEdit.setOnClickListener {
+            val intent = Intent(this, ProfileEditActivity::class.java)
+            startActivity(intent)
+        }
+        binding.buttonSettingNotification.setOnClickListener {
+            val intent = Intent(this, ProfileNotificationActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
