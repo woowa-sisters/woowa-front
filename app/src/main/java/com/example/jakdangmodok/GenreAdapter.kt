c955110a4f356c0c59e40544489bea5059cf8ed9
naahnoey <naahyin20@sookmyung.ac.kr>
2024-01-17 22:56:18 +0900

    Feat: recyclerView와 adapter 연결

---
+++ app/src/main/java/com/example/jakdangmodok/GenreAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.jakdangmodok
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jakdangmodok.databinding.ItemGenreBinding
+
+class GenreViewHolder(val binding: ItemGenreBinding): RecyclerView.ViewHolder(binding.root)
+
+class GenreAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    val genreList = listOf("소설", "시/에세이", "경제/경영", "자기계발", "인문", "역사/문화", "사회", "과학", "예술/대중문화", "종교", "외국어", "기술/공학", "컴퓨터/IT", "취미/스포츠", "건강/다이어트", "가정/육아", "요리", "여행", "교재/수험서", "커리어/수험서", "청소년", "어린이", "만화", "잡지", "해외도서", "오디오북")
+
+    override fun getItemCount(): Int = genreList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            RecyclerView.ViewHolder
+            = GenreViewHolder(ItemGenreBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as GenreViewHolder).binding
+
+        binding.genreButton.text = genreList[position]
+    }
+
+}
--- app/src/main/java/com/example/jakdangmodok/ProfileEditActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/ProfileEditActivity.kt
@@ -2,10 +2,18 @@ package com.example.jakdangmodok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.jakdangmodok.databinding.ActivityProfileEditBinding
 
 class ProfileEditActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityProfileEditBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_edit)
+        setContentView(binding.root)
+
+        binding.recyclerviewGenre.layoutManager = GridLayoutManager(this, 3)
+        binding.recyclerviewGenre.adapter = GenreAdapter()
     }
 }
