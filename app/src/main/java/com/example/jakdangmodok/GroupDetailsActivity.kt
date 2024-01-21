50f9cd6834b181412ab2b6f775c31f260a9cf5bd
naahnoey <naahyin20@sookmyung.ac.kr>
2024-01-21 17:01:59 +0900

    Feat: 멤버 정보 보여주기 - 어댑터 연결

--- app/src/main/java/com/example/jakdangmodok/GroupDetailsActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/GroupDetailsActivity.kt
@@ -2,10 +2,20 @@ package com.example.jakdangmodok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.jakdangmodok.databinding.ActivityGroupDetailsBinding
 
 class GroupDetailsActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityGroupDetailsBinding.inflate(layoutInflater) }
+    private val memberList: ArrayList<String> = arrayListOf("김단비", "연두", "우주")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_group_details)
+        setContentView(binding.root)
+
+        binding.recyclerviewMember.layoutManager = LinearLayoutManager(this)
+        binding.recyclerviewMember.adapter = MemberAdapter(memberList)
     }
+
 }
