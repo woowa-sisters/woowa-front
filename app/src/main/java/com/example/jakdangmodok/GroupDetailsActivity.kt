965c98efdd6df07bdf45bb2dc74711b491f3077e
naahnoey <naahyin20@sookmyung.ac.kr>
2024-01-21 18:29:43 +0900

    Feat: 승인 대기 중 멤버 어댑터 연결

--- app/src/main/java/com/example/jakdangmodok/GroupDetailsActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/GroupDetailsActivity.kt
@@ -8,14 +8,20 @@ import com.example.jakdangmodok.databinding.ActivityGroupDetailsBinding
 class GroupDetailsActivity : AppCompatActivity() {
 
     val binding by lazy { ActivityGroupDetailsBinding.inflate(layoutInflater) }
-    private val memberList: ArrayList<String> = arrayListOf("김단비", "연두", "우주")
+    private val memberList: ArrayList<String> = arrayListOf("김단비", "연두", "우주", "희", "유나", "유진", "유정")
+    private val memberWaitingList: ArrayList<String> = arrayListOf("에이든", "피터", "토니", "스티브", "브루스", "토르", "클린트", "나타샤", "와칸다", "페퍼", "헬라")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        // 멤버 소개
         binding.recyclerviewMember.layoutManager = LinearLayoutManager(this)
         binding.recyclerviewMember.adapter = MemberAdapter(memberList)
+
+        // 가입 신청자
+        binding.recyclerviewMemberWaiting.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        binding.recyclerviewMemberWaiting.adapter = MemberWaitingAdapter(memberList)
     }
 
 }
