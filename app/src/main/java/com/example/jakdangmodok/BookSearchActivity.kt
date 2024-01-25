62dcd3db074560fc1cf62213c47dbbbb1ad431af
naahnoey <naahyin20@sookmyung.ac.kr>
2024-01-26 00:24:04 +0900

    Design: 검색 화면

---
+++ app/src/main/java/com/example/jakdangmodok/BookSearchActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jakdangmodok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_search)
+    }
+}
