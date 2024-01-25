933edada962b76a91e76d39d371cdead181c4854
naahnoey <naahyin20@sookmyung.ac.kr>
2024-01-25 23:28:45 +0900

    Chore: scalars 변환기 라이브러리 추가

--- app/src/main/java/com/example/jakdangmodok/BookFragment.kt
+++ app/src/main/java/com/example/jakdangmodok/BookFragment.kt
@@ -2,15 +2,33 @@ package com.example.jakdangmodok
 
 import android.R
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.jakdangmodok.databinding.FragmentBookBinding
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class BookFragment : Fragment() {
+
+    private val gson = GsonBuilder().setLenient().create()
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("http://www.aladin.co.kr/ttb/api/")
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+    private val ttbkey: String = "ttbnaahyin02016001"
+    private val query: String = "ItemNewSpecial"
 
     private val filterList: Array<String> = arrayOf("최신순", "마감순", "거리순")
     private val bookList: ArrayList<String> = ArrayList<String>()
@@ -25,6 +43,8 @@ class BookFragment : Fragment() {
     ): View? {
         val binding = FragmentBookBinding.inflate(inflater, container, false)
 
+        getBookList(query)
+
         val filterAdapter = ArrayAdapter(requireContext(), R.layout.simple_spinner_item, filterList)
         filterAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
 
@@ -33,6 +53,58 @@ class BookFragment : Fragment() {
         binding.spinnerBook.adapter = filterAdapter
 
         return binding.root
+    }
+
+    private fun getBookList(query: String) {
+        val bookService = retrofit.create(BookService::class.java)
+
+        val call = bookService.getBookList(ttbkey, "Bestseller", "안드로이드")
+
+        call.enqueue(object: Callback<String> {
+            override fun onResponse(
+                call: Call<String>,
+                response: Response<String>
+            ) {
+                if (response.isSuccessful) {
+                    Log.e(TAG, "onResponse: " + response.body());
+                    /*
+                    response.body()?.books?.forEach{
+                        Log.d(TAG, it.toString())
+                    }
+
+
+                    val doc = Jsoup.parse(response.body())
+                    val items = doc.select("item")
+
+                    for (item in items) {
+                        val title = item.select("title").text()
+                        val author = item.select("author").text()
+                        val publisher = item.select("publisher").text()
+                        val pubdate = item.select("pubdate").text()
+                        val cover = item.select("cover").text()
+                        val description = item.select("description").text()
+                        val isbn = item.select("isbn").text()
+                        val priceSales = item.select("priceSales").text()
+                        val priceStandard = item.select("priceStandard").text()
+                        val mileage = item.select("mileage").text()
+                        val link = item.select("link").text()
+
+                        bookList.add(title)
+                    }
+                     */
+                } else {
+                    Log.d("BookFragment", "fail: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.d("BookFragment", "onFailure: ${t.message}")
+            }
+        })
+    }
+
+    companion object{
+        private const val TAG = "MainActivity"
     }
 
 }
