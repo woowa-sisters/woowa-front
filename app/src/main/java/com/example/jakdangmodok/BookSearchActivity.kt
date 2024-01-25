3c1ea1f617ec60827dca09b37aaac964b939d311
naahnoey <naahyin20@sookmyung.ac.kr>
2024-01-26 00:33:46 +0900

    Feat: 검색 페이지로 이동

--- app/src/main/java/com/example/jakdangmodok/BookSearchActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/BookSearchActivity.kt
@@ -2,10 +2,32 @@ package com.example.jakdangmodok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.jakdangmodok.databinding.ActivityBookSearchBinding
 
 class BookSearchActivity : AppCompatActivity() {
+
+    private val binding = ActivityBookSearchBinding.inflate(layoutInflater)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_book_search)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbarSearchBook)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
+
+    // 뒤로가기 버튼
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
