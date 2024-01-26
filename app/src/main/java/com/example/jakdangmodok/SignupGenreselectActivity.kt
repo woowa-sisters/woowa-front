f13d817d3f16116b500a47decb40e216765b97b0
minnnnnnni <asmx6@sookmyung.ac.kr>
2024-01-26 13:33:01 +0900

    design:회원가입 시 장르선택 버튼 kt파일

--- app/src/main/java/com/example/jakdangmodok/SignupGenreselectActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/SignupGenreselectActivity.kt
@@ -2,10 +2,17 @@ package com.example.jakdangmodok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.jakdangmodok.databinding.ActivitySignupGenreselectBinding
 
 class SignupGenreselectActivity : AppCompatActivity() {
+    private val binding by lazy { ActivitySignupGenreselectBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup_genreselect)
+        setContentView(binding.root)
+
+        binding.recyclerviewGenre.layoutManager = GridLayoutManager(this, 3)
+        binding.recyclerviewGenre.adapter = GenreAdapter()
+        binding.recyclerviewGenre.addItemDecoration(GridSpaceItemDecoration(3, 20))
     }
 }
