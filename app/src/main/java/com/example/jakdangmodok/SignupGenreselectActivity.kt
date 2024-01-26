c486e0d37d2101921258694864096b4f2c62abd5
minnnnnnni <asmx6@sookmyung.ac.kr>
2024-01-26 13:22:06 +0900

    add:회원가입 시 장르선택 화면 생성 design:회원가입 시 장르선택 화면 xml 디자인

---
+++ app/src/main/java/com/example/jakdangmodok/SignupGenreselectActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jakdangmodok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupGenreselectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_genreselect)
+    }
+}
