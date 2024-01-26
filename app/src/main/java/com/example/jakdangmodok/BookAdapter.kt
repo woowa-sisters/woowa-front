71457b5abeb7f9d0fe2b8384ff91d00bc95e7ca2
naahnoey <naahyin20@sookmyung.ac.kr>
2024-01-26 21:57:31 +0900

    Feat: 표지 이미지 불러오기

--- app/src/main/java/com/example/jakdangmodok/BookAdapter.kt
+++ app/src/main/java/com/example/jakdangmodok/BookAdapter.kt
@@ -2,7 +2,9 @@ package com.example.jakdangmodok
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.jakdangmodok.databinding.ItemBookBinding
 
 class BookViewHolder(val binding: ItemBookBinding): RecyclerView.ViewHolder(binding.root)
@@ -17,10 +19,15 @@ class BookAdapter(val bookList: List<Book>): RecyclerView.Adapter<RecyclerView.V
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val binding = (holder as BookViewHolder).binding
+        val defaultImage = ContextCompat.getDrawable(binding.root.context, R.drawable.ic_launcher_background)
 
         binding.bookTitle.text = bookList[position].title
         binding.bookAuthor.text = bookList[position].author
-        //binding.bookImage.setImageResource(R.drawable.book)
+        Glide.with(binding.root.context)
+            .load(bookList[position].cover)
+            .error(defaultImage)
+            .fallback(defaultImage)
+            .into(binding.bookImage)
     }
 
 }
