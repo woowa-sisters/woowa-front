b779b133a8378dd07a3a8e0b9d1d2e477ef8c963
naahnoey <naahyin20@sookmyung.ac.kr>
2024-01-27 12:52:21 +0900

    Feat: 지도 연결

--- app/src/main/java/com/example/jakdangmodok/GroupDetailsActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/GroupDetailsActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.MotionEvent
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.jakdangmodok.databinding.ActivityGroupDetailsBinding
+import com.naver.maps.map.NaverMapSdk
 
 class GroupDetailsActivity : AppCompatActivity() {
 
-    val binding by lazy { ActivityGroupDetailsBinding.inflate(layoutInflater) }
+    private val binding by lazy { ActivityGroupDetailsBinding.inflate(layoutInflater) }
+
     private val memberList: ArrayList<String> = arrayListOf("김단비", "연두", "우주", "희", "유나", "유진", "유정")
     private val memberWaitingList: ArrayList<String> = arrayListOf("에이든", "피터", "토니", "스티브", "브루스", "토르", "클린트", "나타샤", "와칸다", "페퍼", "헬라")
     private val groupList: ArrayList<String> = arrayListOf("코딩모임", "영화모임", "독서모임", "게임모임", "운동모임", "요리모임", "여행모임", "공연모임", "음악모임", "봉사모임", "기타모임")
@@ -22,6 +26,8 @@ class GroupDetailsActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbarGroupDetails)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        initNaverMap()
 
         // 모임명
         intent.getStringExtra("groupId")?.let {
@@ -45,6 +51,11 @@ class GroupDetailsActivity : AppCompatActivity() {
         binding.recyclerviewComment.adapter = CommentAdapter(commentList)
     }
 
+    private fun initNaverMap() {
+        NaverMapSdk.getInstance(this).client =
+            NaverMapSdk.NaverCloudPlatformClient(ID)
+    }
+
     // 뒤로가기 버튼
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
@@ -55,6 +66,10 @@ class GroupDetailsActivity : AppCompatActivity() {
 
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    companion object {
+        private const val ID = "sjrtcbmv0g"
     }
 
 }
