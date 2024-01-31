fb02cf8b75b7f1dc4b19d925bd83db621ac3244f
present <present@sookmyung.ac.kr>
2024-01-31 22:03:23 +0900

    Add : 프로필 앱바

--- app/src/main/java/com/example/jakdangmodok/ProfileActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/ProfileActivity.kt
@@ -3,6 +3,7 @@ package com.example.jakdangmodok
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.jakdangmodok.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
@@ -29,18 +30,32 @@ class ProfileActivity : AppCompatActivity() {
         binding.buttonApplyGroup.setOnClickListener {
             val intent = Intent(this, ApplyGroupActivity::class.java)
 
-        binding.buttonMyGroup.setOnClickListener {
-            val intent = Intent(this, MyGroupActivity::class.java)
-            
-            startActivity(intent)
+            binding.buttonMyGroup.setOnClickListener {
+                val intent = Intent(this, MyGroupActivity::class.java)
+
+                startActivity(intent)
+            }
         }
-    }
 
         binding.buttonCompleteList.setOnClickListener {
             val intent = Intent(this, CompleteGroupActivity::class.java)
             startActivity(intent)
         }
 
+    }
+
+    // 뒤로가기 버튼
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+
         }
 
     }
+
+}
