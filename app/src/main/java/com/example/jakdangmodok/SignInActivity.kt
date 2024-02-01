ad4f98cd3834aeb8b505f26c47349e74f61823be
naahnoey <naahyin20@sookmyung.ac.kr>
2024-02-01 11:30:06 +0900

    Design: 로그인 화면

--- app/src/main/java/com/example/jakdangmodok/SignInActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/SignInActivity.kt
@@ -3,6 +3,8 @@ package com.example.jakdangmodok
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.jakdangmodok.databinding.ActivitySignInBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class SignInActivity : AppCompatActivity() {
@@ -12,6 +14,17 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+
+    }
+
+    private fun getGoogleClient(): GoogleSignInClient {
+        val googleSignInOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestServerAuthCode(getString(R.string.google_client_id))
+            .requestEmail()
+            .build()
+
+        return GoogleSignIn.getClient(this, googleSignInOption)
     }
 
 }
