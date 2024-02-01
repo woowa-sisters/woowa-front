9381e85d92dc274c5b2b715b8f25505050649d3f
naahnoey <naahyin20@sookmyung.ac.kr>
2024-02-01 12:11:55 +0900

    Feat: 구글 로그인

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,10 +50,10 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.12.0")    // 이미지로딩 라이브러리
     implementation("com.naver.maps:map-sdk:3.17.0")   // 네이버 지도 라이브러리
     // google play service
-    implementation("com.google.gms:play-services:4.3.15")
-    implementation("com.google.android.gms:play-services-auth:20.5.0")
+    implementation("com.google.gms:google-services:4.3.15")
     implementation("com.google.firebase:firebase-auth:22.0.0")
     implementation("com.google.firebase:firebase-bom:32.0.0")
+    implementation("com.google.android.gms:play-services-auth:20.5.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/jakdangmodok/SignInActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/SignInActivity.kt
@@ -2,26 +2,61 @@ package com.example.jakdangmodok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.jakdangmodok.databinding.ActivitySignInBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.Scope
 
 class SignInActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivitySignInBinding.inflate(layoutInflater) }
+    private val googleSignInClient: GoogleSignInClient by lazy { getGoogleClient() }
+    private val googleAuthLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+
+        try {
+            val account = task.getResult(ApiException::class.java)
+
+            // 이름, 이메일 등이 필요하다면 아래와 같이 account를 통해 각 메소드를 불러올 수 있다.
+            val userName = account.givenName
+            val serverAuth = account.serverAuthCode
+
+            //moveSignUpActivity()
+
+        } catch (e: ApiException) {
+            Log.e(SignInActivity::class.java.simpleName, e.stackTraceToString())
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        addClickListeners()
+    }
 
+    private fun addClickListeners() {
+        binding.llSignIn.setOnClickListener {
+            requestGoogleLogin()
+        }
+    }
+
+    private fun requestGoogleLogin() {
+        googleSignInClient.signOut()
+        val signInIntent = googleSignInClient.signInIntent
+        googleAuthLauncher.launch(signInIntent)
     }
 
     private fun getGoogleClient(): GoogleSignInClient {
         val googleSignInOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestServerAuthCode(getString(R.string.google_client_id))
-            .requestEmail()
+            .requestScopes(Scope("https://www.googleapis.com/auth/pubsub"))
+            .requestServerAuthCode(getString(R.string.google_client_id)) // string 파일에 저장해둔 client id 를 이용해 server authcode를 요청한다.
+            .requestEmail() // 이메일도 요청할 수 있다.
             .build()
 
         return GoogleSignIn.getClient(this, googleSignInOption)
