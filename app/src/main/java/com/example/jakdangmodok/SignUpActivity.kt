4f9238182f747a0ee7d71246bc26949bcf48f533
naahnoey <naahyin20@sookmyung.ac.kr>
2024-02-01 21:06:31 +0900

    Fix: 화면 전환

--- app/src/main/java/com/example/jakdangmodok/SignUpActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/SignUpActivity.kt
@@ -3,14 +3,15 @@ package com.example.jakdangmodok
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.jakdangmodok.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivitySignUpBinding.inflate(layoutInflater) }
-    private val transaction = supportFragmentManager.beginTransaction()
-    private val continueStr = R.string.continue_button.toString()
-    private val completeStr = R.string.complete_button.toString()
+    private val fragments: List<Fragment> by lazy {
+        listOf(UsernameFragment(), SelectGenreFragment())
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,21 +22,22 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun setFragment() {
-        transaction.replace(R.id.sign_up_container, UsernameFragment())
-        transaction.commit()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.sign_up_container, fragments[0])
+            .commit()
     }
 
     private fun addClickListener() {
-        binding.btnContinueSignUp.setOnClickListener {
-            when (binding.btnContinueSignUp.text.toString()) {
-                continueStr -> {
-                    transaction.replace(R.id.sign_up_container, SelectGenreFragment())
-                    transaction.addToBackStack(null)
-                    transaction.commit()
+        binding.btnSignUp.setOnClickListener {
+            when (binding.btnSignUp.text.toString()) {
+                "계속" -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.sign_up_container, fragments[1])
+                        .commit()
 
-                    binding.btnContinueSignUp.text = R.string.complete_button.toString()
+                    binding.btnSignUp.text = "완료"
                 }
-                completeStr -> {
+                "완료" -> {
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                     finish()
