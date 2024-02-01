26824fcc3b5d039ae325d9b1e9354bbc29cfd202
naahnoey <naahyin20@sookmyung.ac.kr>
2024-02-01 12:39:12 +0900

    Rename: usename -> username 오타 수정

--- app/src/main/java/com/example/jakdangmodok/UsernameFragment.kt
+++ app/src/main/java/com/example/jakdangmodok/UsernameFragment.kt
@@ -5,9 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.jakdangmodok.databinding.FragmentUsenameBinding
+import com.example.jakdangmodok.databinding.FragmentUsernameBinding
 
-class UsenameFragment : Fragment() {
+class UsernameFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +17,7 @@ class UsenameFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentUsenameBinding.inflate(inflater, container, false)
+        val binding = FragmentUsernameBinding.inflate(inflater, container, false)
 
         return binding.root
     }
