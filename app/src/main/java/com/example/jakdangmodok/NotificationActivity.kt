23efb64407e91270d2a8375025ede0da7170c8c5
minnnnnnni <asmx6@sookmyung.ac.kr>
2024-02-04 05:30:13 +0900

    feat:알림 아이템 생성, 리사이클러뷰 기능 추가

--- app/src/main/java/com/example/jakdangmodok/NotificationActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/NotificationActivity.kt
@@ -2,10 +2,25 @@ package com.example.jakdangmodok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.jakdangmodok.databinding.ActivityNotificationBinding
 
 class NotificationActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityNotificationBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notification)
+        setContentView(binding.root)
+
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.recyclerviewNotification.layoutManager = layoutManager
+        // todo : binding.recyclerviewNotification.adapter = NotificationAdapter()
+        // 수평 구분선 추가
+        val dividerItemDecoration = DividerItemDecoration(
+            binding.recyclerviewNotification.context,
+            LinearLayoutManager.VERTICAL
+        )
+        binding.recyclerviewNotification.addItemDecoration(dividerItemDecoration)
     }
 }
---
+++ app/src/main/java/com/example/jakdangmodok/NotificationAdapter.kt
@@ -0,0 +1,15 @@
+package com.example.jakdangmodok
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jakdangmodok.databinding.ItemNotificationBinding
+
+class NotificationAdapter {
+    // 어댑터 파일 작성하기
+
+
+
+}
