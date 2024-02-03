0521f05318ef93aab259359ed252ddd9ca62d888
minnnnnnni <asmx6@sookmyung.ac.kr>
2024-02-04 04:43:52 +0900

    feat:받아온 인텐트를 모임 상세보기 화면에 보여주기

--- app/src/main/java/com/example/jakdangmodok/GroupDetailsActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/GroupDetailsActivity.kt
@@ -8,6 +8,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.MotionEvent
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.jakdangmodok.databinding.ActivityGroupDetailsBinding
 import com.naver.maps.map.NaverMapSdk
@@ -51,6 +52,30 @@ class GroupDetailsActivity : AppCompatActivity() {
         // 댓글
         binding.recyclerviewComment.layoutManager = LinearLayoutManager(this)
         binding.recyclerviewComment.adapter = CommentAdapter(commentList)
+
+        // 모임 수정 페이지에서 수정한 정보 가져오기
+        val intent = intent
+        val updatedGroupTitle: String? = intent.getStringExtra("updatedGroupTitle")
+        val dateString: String? = intent.getStringExtra("dateString")
+        val updatedMember: String? = intent.getStringExtra("updatedMember")
+        val updatedGroupInfo: String? = intent.getStringExtra("updatedGroupInfo")
+        val updatedFee: String? = intent.getStringExtra("updatedFee")
+
+        // 받아온 데이터를 텍스트뷰로 보여주기
+        val groupTitle = findViewById<TextView>(R.id.group_title)
+        groupTitle.text = updatedGroupTitle
+        val date = findViewById<TextView>(R.id.textview_date)
+        date.text = dateString
+        val member = findViewById<TextView>(R.id.member_now) // todo: 멤버수 "m / n" 포맷대로 보여주기
+        member.text = updatedMember
+        val groupInfo = findViewById<TextView>(R.id.introduction)
+        groupInfo.text = updatedGroupInfo
+        val fee = findViewById<TextView>(R.id.fee)
+        fee.text = updatedFee
+
+        //TODO : 책 정보, 지도 정보 받아온 데이터를 보여주기
+
+        //TODO : 받아온 데이터를 데이터베이스에 저장..(?)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
