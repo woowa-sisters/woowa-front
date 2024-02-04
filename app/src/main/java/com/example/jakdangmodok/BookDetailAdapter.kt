2908c421ffbaa61508a577d61ec056fc7d39ef73
present <present@sookmyung.ac.kr>
2024-02-04 22:28:41 +0900

    Fix : 책 상세 설명 아이템 추가

--- app/src/main/java/com/example/jakdangmodok/BookDetailAdapter.kt
+++ app/src/main/java/com/example/jakdangmodok/BookDetailAdapter.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jakdangmodok.databinding.ItemBookDetailGroupBinding
-import com.example.jakdangmodok.databinding.ItemBookDetailBinding
 
 class BookDetailViewHolder(val binding: ItemBookDetailGroupBinding): RecyclerView.ViewHolder(binding.root) {
     init {
