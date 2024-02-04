c738485a968fd91e317034a0af2d64a68a868af6
naahnoey <naahyin20@sookmyung.ac.kr>
2024-02-04 20:18:05 +0900

    Feat: 검색 결과 5개

--- app/src/main/java/com/example/jakdangmodok/NaverMapService.kt
+++ app/src/main/java/com/example/jakdangmodok/NaverMapService.kt
@@ -11,6 +11,7 @@ interface NaverMapService {
     @GET("/v1/search/local.json")
     suspend fun getMapSearch(
         @Query("query") query: String,      // 검색어
+        @Query("display") display: Int = 5,    // 검색 결과 출력 건수
         @Header("X-Naver-Client-Id") clientId: String = "IEVypWnAYI9BtTLNGSFw",    // 검색 클라이언트 아이디
         @Header("X-Naver-Client-Secret") clientSecret: String = "jltSefe3KN",     // 검색 클라이언트 시크릿
     ): Response<PlaceListDTO>
