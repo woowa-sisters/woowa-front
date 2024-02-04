d3db915535346fe3e0ddb42eb31b1a4d6548d75a
minnnnnnni <asmx6@sookmyung.ac.kr>
2024-02-04 17:46:00 +0900

    FEAT:알림 모음 기능 어댑터 작성 및 kt파일 작성 DESIGN:알림 모음 XML 디자인 수정

--- app/src/main/java/com/example/jakdangmodok/NotificationActivity.kt
+++ app/src/main/java/com/example/jakdangmodok/NotificationActivity.kt
@@ -4,18 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.jakdangmodok.databinding.ActivityNotificationBinding
+
+// 데이터 클래스 생성하기. (text: 알림 메시지 문자열, time: 작성 시간 문자열)
+data class NotificationItem(val text: String, val date: String)
 
 class NotificationActivity : AppCompatActivity() {
     private val binding by lazy { ActivityNotificationBinding.inflate(layoutInflater) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        binding.recyclerviewNotification.layoutManager = layoutManager
-        // todo : binding.recyclerviewNotification.adapter = NotificationAdapter()
+
+        val rv_notice = findViewById<RecyclerView>(R.id.recyclerview_notification)
+
+        val itemList = ArrayList<NotificationItem>()
+        itemList.add(NotificationItem("xx모임의 모임 승인 요청이 완료되었습니다.", "02/02 1:31")) // 모임 승인 요청[그룹원]
+        itemList.add(NotificationItem("00님이 xx모임에 모임 승인을 요청하였습니다.", "02/02 1:31")) // 모임 승인 요청[모임장]
+        itemList.add(NotificationItem("xx모임이 관심 모임으로 등록되었습니다.", "02/02 1:31")) // 관심 모임[그룹원]
+        itemList.add(NotificationItem("00님이 xx모임을 관심 모임으로 등록하였습니다.", "02/02 1:31")) // 관심 모임[모임장]
+        itemList.add(NotificationItem("00님이 xx모임에 댓글을 작성하였습니다.", "02/02 1:31")) // 댓글 알림[아마 모임장한테만 알림 가도록]
+
+
+        val notificationAdapter = NotificationAdapter(itemList)
+        notificationAdapter.notifyDataSetChanged()
+
+        rv_notice.adapter = notificationAdapter
+        rv_notice.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+
+        //binding.recyclerviewNotification.adapter = NotificationAdapter()
+
         // 수평 구분선 추가
         val dividerItemDecoration = DividerItemDecoration(
             binding.recyclerviewNotification.context,
--- app/src/main/java/com/example/jakdangmodok/NotificationAdapter.kt
+++ app/src/main/java/com/example/jakdangmodok/NotificationAdapter.kt
@@ -2,14 +2,40 @@ package com.example.jakdangmodok
 
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jakdangmodok.databinding.ItemNotificationBinding
 
-class NotificationAdapter {
-    // RecyclerView 아이템 크기
+
+
+class NotificationAdapter(val itemList: ArrayList<NotificationItem>) :
+    RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_notification, parent, false)
+        return NotificationViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NotificationViewHolder, position: Int) {
+        holder.n_message.text = itemList[position].text
+        holder.n_date.text = itemList[position].date
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.count()
+    }
+
+
+    class NotificationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val n_message  = itemView.findViewById<TextView>(R.id.notice_message)
+        val n_date = itemView.findViewById<TextView>(R.id.notice_date)
+    }
+
+
+    }
 
 
 
-}
